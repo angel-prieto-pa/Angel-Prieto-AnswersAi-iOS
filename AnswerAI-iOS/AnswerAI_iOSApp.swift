8846bf48ef21eba80402d985c95b98452c007d40
angel-prieto-pa <angel.prieto.pa@gmail.com>
2024-12-03 10:32:43 -0800

    Initial Commit

---
+++ AnswerAI-iOS/AnswerAI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnswerAI_iOSApp.swift
+//  AnswerAI-iOS
+//
+//  Created by Angel Prieto on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AnswerAI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnswerAI-iOSTests/AnswerAI_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnswerAI_iOSTests.swift
+//  AnswerAI-iOSTests
+//
+//  Created by Angel Prieto on 12/3/24.
+//
+
+import Testing
+
+struct AnswerAI_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
